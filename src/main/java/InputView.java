8202c9a2e4142973de0b8acee400b3052fab5cfd
sungminyun <sungmin.yun@nflux.co.kr>
2024-08-01 18:48:03 +0900

    feat: 사용자 입력 문구 출력
    사용자가 입력한 수를 입력받는다

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -7,4 +7,8 @@ public class InputView {
         return Integer.parseInt(scanner.nextLine()) == 1;
     }
 
+    public int getUserInput(){
+        System.out.print("숫자를 입력해 주세요 : ");
+        return Integer.parseInt(scanner.nextLine());
+    }
 }
